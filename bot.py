9891c1feb9bbba948e98bd09efb04aa1d299c432
Ruslan <ruslanalmasovich@gmail.com>
2024-11-03 16:40:18 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -22,9 +22,21 @@ TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
 CMC_API_KEY = os.getenv("CMC_API_KEY")
 WEBHOOK_URL = "https://botcriptan.onrender.com"  # URL на Render
 
-# Инициализация Firebase с использованием учетных данных из переменной окружения
-firebase_credentials = json.loads(os.getenv("FIREBASE_CREDENTIALS"))
-print("Firebase credentials загружены корректно:", firebase_credentials)  # Для отладки
+# Инициализация Firebase с использованием отдельных переменных окружения
+firebase_credentials = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
+}
+
+# Инициализация Firebase
 cred = credentials.Certificate(firebase_credentials)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
